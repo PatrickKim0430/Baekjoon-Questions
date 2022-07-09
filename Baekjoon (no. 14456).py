73f6cbd22bbdb07749bd702da53773b524cf18cc
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-07-09 11:41:23 +0900

    Baekjoon no. 11047 (Coin 0)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/7/9
    # Problem: Baekjoon no. 11047 (Coin 0)  
    https://www.acmicpc.net/problem/11047
    # Solution: Using lists concepts to solve this problem

--- Baekjoon (no. 14456).py
+++ Baekjoon (no. 14456).py
@@ -1,35 +1,22 @@
 # Programmer:  Patrick Junhee Kim 
-# Date: 2022/7/8
-# Problem: USACO bronze problem (2017 january contest, problem 2) 
-# Baekjoon 14456 https://www.acmicpc.net/problem/14456
-# Solution: Using if, elif, and else statements to solve this problem
+# Date: 2022/7/9
+# Problem: Baekjoon no. 11047, coin 0  
+https://www.acmicpc.net/problem/11047
+# Solution: Using lists concepts to solve this problem
 
-
-
-n=int(input())
-# 1: scissors
-# 2: hoof
-# 3: papers
+a,b=input().split()
+a=int(a)
+b=int(b)
+l=[]
 count=0
-loss=0
-for i in range(n):
-    a,b=input().split()
-    a=int(a)
-    b=int(b)
-    if a == 1 and b ==3:
-        count = count +1
-    elif a == 1 and b==2:
-        loss=loss+1
-    elif a == 2 and b == 1:
-        count = count +1
-    elif a ==2 and b ==3:
-        loss = loss+1
-    elif a == 3 and b == 2:
-        count = count +1
-    elif a ==3 and b ==1:
-        loss = loss +1
-        
-if loss > count: 
-    print(loss)
-else:
-    print(count)
+for i in range(a):
+    n=int(input())
+    l.append(n)
+    
+l.sort(reverse=True)
+
+for i in l:
+    count=count+(b//i)
+    b=b%i
+
+print(count)
