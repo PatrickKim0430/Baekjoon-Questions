539272aa69369683e90c68a32d4da9f4a3186b51
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-04 23:24:24 +0900

    Baekjoon no. 10869 (the four fundamental arithmetic operations)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/8/4
    # Problem: Baekjoon no. 10869, the four fundamental arithmetic operations
    https://www.acmicpc.net/problem/10869
    # Solution: Using int(input()), int concept to solve this problem

---
+++ Baekjoon (no. 10869).py
@@ -0,0 +1,12 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/8/4
+# Problem: Baekjoon no. 10869, the four fundamental arithmetic operations
+https://www.acmicpc.net/problem/10869
+# Solution: Using int(input()), int concept to solve this problem
+
+a,b=input().split() 
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)//int(b))
+print(int(a)%int(b))
