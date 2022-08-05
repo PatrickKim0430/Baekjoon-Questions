6f9daf4ad2c6d35ecfbb4d1f4b38d0a73e7c32c7
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-05 21:19:19 +0900

    Baekjoon no. 2588 (multiplication)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/8/5
    # Problem: Baekjoon no. 2588, multiplication
    https://www.acmicpc.net/problem/2588
    # Solution: Using int(input()), print concept to solve this problem

---
+++ Baekjoon (no. 2588).py
@@ -0,0 +1,13 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/8/5
+# Problem: Baekjoon no. 2588, multiplication
+https://www.acmicpc.net/problem/2588
+# Solution: Using int(input()), print concept to solve this problem
+
+a=int(input())
+b=input()
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
