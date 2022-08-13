cf77e03a85d7272e7a1ea6af01515e500fcdb5c2
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-08-13 22:59:04 +0900

    Baekjoon no. 11654 (ASCII code)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/8/13
    # Problem: Baekjoon no. 11654, ASCII code
    https://www.acmicpc.net/problem/11654
    # Solution: Using int(input()), statement concept to solve this problem

---
+++ Baekjoon (no. 11654).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/8/13
+# Problem: Baekjoon no. 11654, ASCII code
+https://www.acmicpc.net/problem/11654
+# Solution: Using int(input()), statement concept to solve this problem
+
+a=input()
+print(ord(a))
