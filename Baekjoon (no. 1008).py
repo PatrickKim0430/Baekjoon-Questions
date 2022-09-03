573fb5ef0e576d68f3efa403593bed81862f4a4b
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-03 21:07:04 +0900

    Baekjoon no. 1008 (A/B)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/3
    # Problem: Baekjoon no. 1008, A/B
    https://www.acmicpc.net/problem/1008
    # Solution: Using input().split() concept to solve this problem

---
+++ Baekjoon (no. 1008).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/3
+# Problem: Baekjoon no. 1008, A/B
+https://www.acmicpc.net/problem/1008
+# Solution: Using input().split() concept to solve this problem
+
+a,b=input().split()
+print(int(a)/int(b))
