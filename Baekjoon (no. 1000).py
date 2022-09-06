004dc403698b91991be269a7257d840ffb6449f4
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-06 23:38:56 +0900

    Baekjoon no. 1000 (A+B)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/6
    # Problem: Baekjoon no. 1000, A+B
    https://www.acmicpc.net/problem/1000
    # Solution: Using input().split() concept to solve this problem

---
+++ Baekjoon (no. 1000).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/6
+# Problem: Baekjoon no. 1000, A+B
+https://www.acmicpc.net/problem/1000
+# Solution: Using input().split() concept to solve this problem
+
+A,B=input().split()
+print(int(A)+int(B))
