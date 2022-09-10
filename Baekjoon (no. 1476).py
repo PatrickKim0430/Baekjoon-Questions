858777486ed885eaf014bde1ab419069f0de621d
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-10 20:57:18 +0900

    Baekjoon no. 1476 (Date calculation)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/10
    # Problem: Baekjoon no. 1476, date calculation 
    https://www.acmicpc.net/problem/1476
    # Solution: Using map, while loop concept to solve this problem

---
+++ Baekjoon (no. 1476).py
@@ -0,0 +1,21 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/10
+# Problem: Baekjoon no. 1476, date calculation 
+https://www.acmicpc.net/problem/1476
+# Solution: Using map, while loop concept to solve this problem
+
+E,S,M,cnt =1,1,1,1
+
+i_E , i_S , i_M = map(int,input().split())
+
+while(True):
+    if i_E==E and i_S==S and i_M==M :
+        break
+    E+=1 ; S+=1 ; M+=1; cnt+=1
+    if E>=16 : E-=15
+    if S>=29 : S-=28
+    if M>=20 : M-=19
+
+
+
+print(cnt)
