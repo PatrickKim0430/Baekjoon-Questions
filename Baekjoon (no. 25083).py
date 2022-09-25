a81d569c07bb9afed5229fd75dcc5411dbd02c6c
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-09-25 23:14:58 +0900

    Baekjoon no. 25083 (Sprout)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/9/25
    # Problem: Baekjoon no. 25083, Sprout
    https://www.acmicpc.net/problem/25083
    # Solution: Using print concept to solve this problem

---
+++ Baekjoon (no. 25083).py
@@ -0,0 +1,13 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/9/25
+# Problem: Baekjoon no. 25083, Sprout
+https://www.acmicpc.net/problem/25083
+# Solution: Using print concept to solve this problem
+
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
