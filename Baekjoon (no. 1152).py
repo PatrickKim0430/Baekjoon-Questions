1b751a281d1817cded27fcc7fb1fd79617fc30a3
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-10-22 23:47:21 +0900

    Baekjoon no. 1152 (Number of words)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/10/22
    # Problem: Baekjoon no. 1152, Number of words
    https://www.acmicpc.net/problem/1152
    # Solution: Using input().split() concept to solve this problem

---
+++ Baekjoon (no. 1152).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/10/22
+# Problem: Baekjoon no. 1152, Number of words
+https://www.acmicpc.net/problem/1152
+# Solution: Using input().split() concept to solve this problem
+
+word = input().split()
+print(len(word))
