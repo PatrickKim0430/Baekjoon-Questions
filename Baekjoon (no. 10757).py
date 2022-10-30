2798eca0c7fcfb95ff1e628d563ff4b5e220c148
Patrick Junhee Kim <83442906+PatrickKim0430@users.noreply.github.com>
2022-10-30 23:58:03 +0900

    Baekjoon 10757 (Large number A+B)
    # Programmer:  Patrick Junhee Kim 
    # Date: 2022/10/30
    # Problem: Baekjoon 10757, Large number A+B
    # Baekjoon 10757 https://www.acmicpc.net/problem/10757
    # Solution: Using map(int,input().split()) to solve this problem

---
+++ Baekjoon (no. 10757).py
@@ -0,0 +1,8 @@
+# Programmer:  Patrick Junhee Kim 
+# Date: 2022/10/30
+# Problem: Baekjoon 10757, Large number A+B
+# Baekjoon 10757 https://www.acmicpc.net/problem/10757
+# Solution: Using map(int,input().split()) to solve this problem
+
+A, B = map(int, input().split())
+print(A+B)
